4f99f7bb3d2827aa7f5bcdf558d16d237d996b80
Jonatan <jonatanruedasmora@gmail.com>
2020-01-24 15:34:49 +0100

    Updates MyClass to use the new implementation of the Time class

--- src/main/java/es/upm/grise/profundizacion2019/ex3/MyClass.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/MyClass.java
@@ -1,9 +1,12 @@
 package es.upm.grise.profundizacion2019.ex3;
 
+import java.time.LocalDateTime;
+
 public class MyClass {
 	
 	public void nextTime(long seconds) {
-		String next = Time.getFutureTime(seconds);
+		Time time = new Time(LocalDateTime.now());
+		String next = time.getFutureTime(seconds);
 		System.out.println(next);
 	}
 
